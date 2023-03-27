69802090884b5cd2e176c637cbbf7a19ca41f953
Hasmelash95 <hermonasmelash@yahoo.com>
2023-03-27 13:49:51 +0000

    Add hypothesis and validation to dashboard

--- app_pages/page_hypothesis.py
+++ app_pages/page_hypothesis.py
@@ -1,4 +1,25 @@
 import streamlit as st
 
 def project_hypothesis_page():
-    st.write("Hypothesis page")
+    st.title("Hypothesis and Validation")
+
+    st.markdown("1. We believe that mildew containing cherry leaves"
+                " can be detected by the white specks covering"
+                " the leaf's surface.")
+    
+    st.success("Using average image and image variability plots"
+               " we were able to confirm this. White specks on"
+               " on the surface of the leaf is the distinguishing"
+               " feature between healthy and powdery mildew containing"
+               " cherry leaves.")
+
+    st.markdown("2. We believe a model can be trained"
+                " to determine weather a given cherry leaf is"
+                " healthy or contains powdery mildew with a"
+                " degree of 97% accuracy.")
+    
+    st.success("A model has been successfully trained to"
+               " to class a given image (of a cherry leaf) as being"
+               " healthy or containing mildew with a degree of accuracy"
+               " above 99%.")
+
