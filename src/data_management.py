0437cb92f87e3dc421ca6d7a10a36cebfdaad345
Hasmelash95 <hermonasmelash@yahoo.com>
2023-03-27 13:55:37 +0000

    Create function to download dataframe as csv file

---
+++ src/data_management.py
@@ -0,0 +1,22 @@
+import numpy as np
+import pandas as pd
+import os
+import base64
+from datetime import datetime
+import joblib
+
+def download_df_as_csv(df):
+    """
+    Function to download a dataframe as a 
+    csv file.
+    """
+    present_date = datetime.now().strftime("%d%m%Y_%Hh%Mmin%Ss")
+    csv = df.to_csv().encode()
+    b64 = base64.b64encode(csv).decode()
+    href = (
+        f'<a href="data:file/csv;base64,{b64}" download="Report {present_date}.csv" '
+        f'target="_blank">Download Report</a>'
+    )
+
+    return href
+
